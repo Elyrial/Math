d6fd69ea92cc4e1f04edeebf5220a99650b1b9a0
Elyrial <150860379+Elyrial@users.noreply.github.com>
2023-11-16 17:15:07 +0100

    Update and rename solution.cpp to WeakVerticies.cpp

--- WeakVerticies.cpp
+++ WeakVerticies.cpp
@@ -13,18 +13,10 @@
 #define read(x) int x; cin >> x
 
 using namespace std;
- 
-void init_code(){
-    fast_io;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif 
-}
+
 
  
 int main() {
-   init_code();
    read(n);
    while(n>0) {
       int graph[n][n];
